b0402b7c1a3c07455cc0958fcc9de709023336b6
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-01-14 02:01:10 +0800

    Added Token Model

---
+++ backend/app/database/models/token.ts
@@ -0,0 +1,20 @@
+import { Column, DataType, ForeignKey, Model, Table } from "sequelize-typescript";
+
+import { IToken } from "../../interfaces/model.columns/IToken";
+
+import { User } from "./user";
+
+@Table({
+  tableName: "tokens",
+  paranoid: true
+})
+export class Token extends Model<Token> implements IToken {
+  @Column(DataType.STRING)
+  public token: string;
+
+  @ForeignKey(() => User)
+  @Column(DataType.STRING)
+  public userId?: string;
+}
+
+export default Token;
