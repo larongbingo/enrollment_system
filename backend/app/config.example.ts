6eeeed49a13f3f622e0f243fb03821e62161db77
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-01-14 00:58:50 +0800

    Added config.example file

---
+++ backend/app/config.example.ts
@@ -0,0 +1,16 @@
+/**
+ * Copy this file or the contents and replace the texts within
+ * the angled brackets with their corresponding value
+ */
+
+export const DATABASE_CONFIG = {
+  database: '<name of the database in the mysql server>',
+  username: '<username of the account in the mysql server>',
+  password: '<password of the account in the mysql server>'
+};
+
+const config = {
+  database_config: DATABASE_CONFIG
+};
+
+export default config;
