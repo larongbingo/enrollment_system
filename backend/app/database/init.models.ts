3d487b770b62e6ac96e51f5aae03dd3336b18c75
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-01-14 00:59:10 +0800

    Added init of models

---
+++ backend/app/database/init.models.ts
@@ -0,0 +1,10 @@
+import { Sequelize } from 'sequelize-typescript';
+import User from './models/user';
+
+export function initModels(sequelize: Sequelize) {
+  sequelize.addModels([
+    User
+  ]);
+}
+
+export default initModels;
