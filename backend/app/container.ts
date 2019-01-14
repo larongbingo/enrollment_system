e8c7610c80d2913e7f1434210250e12091b14c57
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-01-14 10:49:24 +0800

    Added TokenDataAccess and UserDataAccess to container

--- backend/app/container.ts
+++ backend/app/container.ts
@@ -1,5 +1,15 @@
 import { Container } from "inversify";
 
+import CONSTANTS from "./constants";
+import { TokenDataAccess, UserDataAccess } from "./data.access";
+import Token from "./database/models/token";
+import User from "./database/models/user";
+import { IAuth } from "./interfaces/IAuth";
+import { IDataAccess } from "./interfaces/IDataAccess";
+
 export const CONTAINER = new Container();
+
+CONTAINER.bind<IDataAccess<Token>>(CONSTANTS.dataAccess.Token).to(TokenDataAccess).inSingletonScope();
+CONTAINER.bind<IDataAccess<User>>(CONSTANTS.dataAccess.User).to(UserDataAccess).inSingletonScope();
 
 export default CONTAINER;
