d58ba3b22456b3044c9a582a5e2fa7f8e116de5c
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-01-14 14:57:00 +0800

    Added jwt key on config.sample

--- backend/app/config.example.ts
+++ backend/app/config.example.ts
@@ -6,16 +6,17 @@
 export const DATABASE_CONFIG = {
   database: "<name of the database in the mysql server>",
   username: "<username of the account in the mysql server>",
-  password: "<password of the account in the mysql server>"
+  password: "<password of the account in the mysql server>",
 };
 
 export const AUTH_CONFIG = {
-  bcrypt_secret: "<a word or something very long>"
+  bcrypt_secret: "<a word or something very long>",
+  jwt_key: "<a word or something very large>",
 };
 
 const config = {
   DATABASE_CONFIG,
-  AUTH_CONFIG
+  AUTH_CONFIG,
 };
 
 export default config;
