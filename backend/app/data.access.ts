d27686e508be0f8672bb9c65219b911428fceac4
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-01-14 10:41:19 +0800

    Added dataAcess for Token

--- backend/app/data.access.ts
+++ backend/app/data.access.ts
@@ -3,8 +3,11 @@ import { ICreateOptions, IFindOptions } from "sequelize-typescript";
 // tslint:disable-next-line:no-submodule-imports
 import { FilteredModelAttributes } from "sequelize-typescript/lib/models/Model";
 
+import Token from "./database/models/token";
 import User from "./database/models/user";
 import { IDataAccess } from "./interfaces/IDataAccess";
+
+// tslint:disable:max-classes-per-file
 
 @injectable()
 export class UserDataAccess implements IDataAccess<User> {
@@ -18,5 +21,20 @@ export class UserDataAccess implements IDataAccess<User> {
 
   public async findAll(options?: IFindOptions<User>) {
     return User.findAll(options);
+  }
+}
+
+@injectable()
+export class TokenDataAccess implements IDataAccess<Token> {
+  public async create(values?: FilteredModelAttributes<Token>, options?: ICreateOptions) {
+    return Token.create(values, options);
+  }
+
+  public async findOne(options?: IFindOptions<Token>) {
+    return Token.findOne(options);
+  }
+
+  public async findAll(options?: IFindOptions<Token>) {
+    return Token.findAll(options);
   }
 }
