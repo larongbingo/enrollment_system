b7b531859926ada9dbfe60ccd7da877df528cf5c
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-01-14 10:43:58 +0800

    Added constants for token and auth

--- backend/app/constants.ts
+++ backend/app/constants.ts
@@ -1,6 +1,10 @@
 export const CONSTANTS = {
   dataAccess: {
     User: Symbol.for("UserDataAccess"),
+    Token: Symbol.for("TokenDataAccess"),
+  },
+  services: {
+    Authentication: Symbol.for("Authentication"),
   },
 };
 
