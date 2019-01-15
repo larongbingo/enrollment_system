85ef68124b8b44b639bbfa2222a537beb8e4fe73
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-01-15 17:15:41 +0800

    Added token model to init models

--- backend/app/database/init.models.ts
+++ backend/app/database/init.models.ts
@@ -1,9 +1,11 @@
 import { Sequelize } from "sequelize-typescript";
 
+import Token from "./models/token";
 import User from "./models/user";
 
 export function initModels(sequelize: Sequelize) {
   sequelize.addModels([
+    Token,
     User
   ]);
 }
