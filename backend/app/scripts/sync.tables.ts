07b410854e4bb0e074f85bacf896cba9f688138d
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-01-16 16:01:49 +0800

    Added a script to sync models to MySQL server

---
+++ backend/app/scripts/sync.tables.ts
@@ -0,0 +1,26 @@
+import { sequelize } from "../database/connection";
+
+(async function() {
+  if(process.env.NODE_ENV === "production") {
+    console.log("CANNOT RUN THIS ON A PRODUCTION SERVER;");
+    process.exit(1);
+  }
+  
+  try {
+    // TODO: FIND AN ELEGANT SOLUTION TO DROP TABLES WITH RELATIONSHIPS
+    console.log("DROPPING ALL PREVIOUS TABLES");
+    await sequelize.query(`DROP DATABASE ${sequelize.options.database};`);
+    await sequelize.query(`CREATE DATABASE ${sequelize.options.database};`);
+    await sequelize.query(`USE ${sequelize.options.database};`);
+
+    console.log("UPDATING TABLES");
+    await sequelize.sync({force: true});
+  }
+  catch(err) {
+    console.error(err);
+    process.exit(2);
+  }
+
+  console.log("DONE");
+  process.exit(0);
+})();
