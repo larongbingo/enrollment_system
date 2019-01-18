629ca0c3786561745407243b60baa376227f8262
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-01-18 13:53:25 +0800

    Allow createToken to return null

--- backend/app/interfaces/ISessionManager.ts
+++ backend/app/interfaces/ISessionManager.ts
@@ -4,7 +4,7 @@ import IUser from "./model.columns/IUser";
  * Manages creation and deletion of JWT Session keys
  */
 export interface ISessionManager {
-  createToken(details: IUser): Promise<string>;
+  createToken(details: IUser): Promise<string | null>;
   destroyToken(token: string): Promise<void>;
   verifyToken(token: string): Promise<boolean>;
 }
