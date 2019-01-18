77a48d395424e1c4838e283dcf0affd91f311977
larongbingo <rcyapagulayan@gmail.com>
2019-01-18 13:46:18 +0800

    MOVING CHANGES

--- backend/tests/services/ArraySessionManager.spec.ts
+++ backend/tests/services/ArraySessionManager.spec.ts
@@ -1,12 +1,36 @@
-import { expect } from "chai";
+import { expect, use } from "chai";
 import "mocha";
-import {} from "sinon";
+import { fake } from "sinon";
 import sinonChai from "sinon-chai";
 
-import { ArraySessionManager } from "../../app/services/ArraySessionManager";
+import { CONSTANTS } from "../../app/constants";
+import { CONTAINER } from "../../app/container";
+import { ISessionManager } from "../../app/interfaces/ISessionManager";
+
+// tslint:disable:no-unused-expression
+
+use(sinonChai);
 
 describe("ArraySessionManager Service", function() {
+  let S: ISessionManager;
+
+  before(function() {
+    S = CONTAINER.get<ISessionManager>(CONSTANTS.services.SessionManager);
+  });
+
   it("should return an object when Container.get was called with the key", function() {
     expect(S).to.not.be.null;
+  });
+
+  it("should have a function named createToken", function() {
+    expect(typeof S.createToken).to.be.equal("Function");
+  });
+
+  it("should have a function named destroyToken", function() {
+    expect(typeof S.destroyToken).to.be.equal("Function");
+  });
+  
+  it("should have a function named verifyToken", function() {
+    expect(typeof S.verifyToken).to.be.equal("Function");
   });
 });
