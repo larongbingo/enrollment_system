25c118c9df171718df218bda9890069a4de5f5af
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-01-19 22:11:22 +0800

    Added user deletion

--- backend/app/interfaces/IAccountRegistry.ts
+++ backend/app/interfaces/IAccountRegistry.ts
@@ -8,12 +8,19 @@ export interface IAccountRegistry {
    * Creates a new user
    * @param details The details for the new user
    */
-  createUser(details: IUser): Promise<IUser>;
+  createUser(details: IUser): Promise<IUser | null>;
   
   /**
    * Updates the user using the id 
    * @param newDetails The new details of the user
    * @param userId The id of the user
    */
-  updateUser(newDetails: IUser, userId: string): Promise<IUser>;
+  updateUser(newDetails: IUser, userId: string): Promise<IUser | null>;
+
+  /**
+   * Marks the user as deleted and revokes access to the details and data associated with the user
+   * @param userId The id of the user that needs to be deleted
+   * @returns Returns true if user was marked as deleted; false otherwise
+   */
+  deleteUser(userId: string): Promise<boolean>;
 }
