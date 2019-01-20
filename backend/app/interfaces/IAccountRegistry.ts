bbde5d1522160263ea642fddbbe50442e3961e4a
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-01-20 22:43:01 +0800

    Added functions to allow storage of personal details of user

--- backend/app/interfaces/IAccountRegistry.ts
+++ backend/app/interfaces/IAccountRegistry.ts
@@ -1,4 +1,5 @@
 import { IUser } from "./model.columns/IUser";
+import { IUserDetails } from "./model.columns/IUserDetails";
 
 /**
  * Handles update and creation of users
@@ -25,4 +26,20 @@ export interface IAccountRegistry {
    * @returns Returns true if user was marked as deleted; false otherwise
    */
   deleteUser(userId: string): Promise<boolean>;
+
+  /**
+   * Stores the given personal details and links it to the given user id
+   * @param personalDetails The personal details of the user
+   * @param userId The id of the user to create
+   * @returns Returns true if the details was stored; false otherwise
+   */
+  createPersonalDetails(personalDetails: IUserDetails, userId: string): Promise<boolean>;
+
+  /**
+   * Updates the stored details with the new details given
+   * @param newDetails The new details of the user
+   * @param userId The id of the user that needs to update
+   * @returns Returns an instance of the details if it was updated; null otherwise
+   */
+  updatePersonalDetails(newDetails: IUserDetails, userId: string): Promise<IUserDetails | null>;
 }
