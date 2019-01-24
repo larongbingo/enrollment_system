dbc66e0aeb8c2b8bb1e04b5f4df1ae19ae0987a4
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-01-24 18:27:47 +0800

    backend AccountRegistry: Implemented functions on personal details

--- backend/app/services/AccountRegistry.ts
+++ backend/app/services/AccountRegistry.ts
@@ -3,10 +3,12 @@ import { Op } from "sequelize";
 
 import CONSTANTS from "../constants";
 import User from "../database/models/user";
+import UserDetails from "../database/models/user.details";
 import { IAccountRegistry } from "../interfaces/IAccountRegistry";
 import { IDataAccess } from "../interfaces/IDataAccess";
 import { IUserValidations } from "../interfaces/IUserValidations";
 import { IUser } from "../interfaces/model.columns/IUser";
+import { IUserDetails } from "../interfaces/model.columns/IUserDetails";
 
 @injectable()
 export class AccountRegistry implements IAccountRegistry {
@@ -52,6 +54,41 @@ export class AccountRegistry implements IAccountRegistry {
     }
     catch(err) { return false; }
     return true;
+  }
+
+  public async createPersonalDetails(personalDetails: IUserDetails, userId: string): Promise<boolean> {
+    if(!personalDetails || !userId) { return false; }
+    try {
+      let user = await this._userAccess.findOne({where: {id: {[Op.eq]: userId}}});
+      if(!user) { return false; }
+
+      {
+        let details = await user!.$get<UserDetails>("details");
+        if(details) { return false; }
+      }
+
+      user!.$create<UserDetails>("details", personalDetails);
+    }
+    catch(err) { return false; }
+    return true;
+  }
+  
+  public async updatePersonalDetails(newDetails: IUserDetails, userId: string): Promise<IUserDetails | null> {
+    if(!newDetails || !userId) { return null; }
+    let details: UserDetails;
+    try {
+      let user = await this._userAccess.findOne({where: {id: {[Op.eq]: userId}}});
+      if(!user) { return null; }
+
+      details = await user!.$get<UserDetails>("details") as UserDetails;
+      if(!details) { return null; }
+      
+      // @ts-ignore
+      Object.keys(newDetails).forEach((key) => details[key] = newDetails[key]);
+      await details.save();
+    }
+    catch(err) { return null; }
+    return details;
   }
 }
 
