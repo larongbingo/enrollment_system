a9d9196f652adfbdfb482080dda6e6811ff23e01
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-01-25 21:39:26 +0800

    backend data.access: Implemented the DataAccess to the new models

--- backend/app/data.access.ts
+++ backend/app/data.access.ts
@@ -5,6 +5,10 @@ import { ICreateOptions, IFindOptions } from "sequelize-typescript";
 import { FilteredModelAttributes } from "sequelize-typescript/lib/models/Model";
 
 import AcademicTime from "./database/models/academic.time";
+import RequiredSession from "./database/models/required.session";
+import Room from "./database/models/room";
+import Schedule from "./database/models/schedule";
+import Session from "./database/models/session";
 import Token from "./database/models/token";
 import User from "./database/models/user";
 import UserDetails from "./database/models/user.details";
@@ -70,5 +74,62 @@ export class AcademicTimeDataAccess implements IDataAccess<AcademicTime> {
 
   public async findAll(options?: IFindOptions<AcademicTime> | undefined): Promise<AcademicTime[]> {
     return AcademicTime.findAll(options);
+  }
+}
+
+export class RequiredSessionDataAccess implements IDataAccess<RequiredSession> {
+  public async create
+  (values?: FilteredModelAttributes<RequiredSession>, options?: ICreateOptions): Promise<RequiredSession> {
+    return RequiredSession.create(values, options);
+  }
+  
+  public async findOne(options?: IFindOptions<RequiredSession> | undefined): Promise<RequiredSession | null> {
+    return RequiredSession.findOne(options);
+  }
+
+  public async findAll(options?: IFindOptions<RequiredSession> | undefined): Promise<RequiredSession[]> {
+    return RequiredSession.findAll(options);
+  }
+}
+
+export class RoomDataAccess implements IDataAccess<Room> {
+  public async create(values?: FilteredModelAttributes<Room>, options?: ICreateOptions): Promise<Room> {
+    return Room.create(values, options);
+  }  
+  
+  public async findOne(options?: IFindOptions<Room> | undefined): Promise<Room | null> {
+    return Room.findOne(options);
+  }
+  
+  public async findAll(options?: IFindOptions<Room> | undefined): Promise<Room[]> {
+    return Room.findAll(options);
+  }
+}
+
+export class ScheduleDataAccess implements IDataAccess<Schedule> {
+  public async create(values?: FilteredModelAttributes<Schedule>, options?: ICreateOptions): Promise<Schedule> {
+    return Schedule.create(values, options);
+  }  
+  
+  public async findOne(options?: IFindOptions<Schedule> | undefined): Promise<Schedule | null> {
+    return Schedule.findOne(options);
+  }
+  
+  public async findAll(options?: IFindOptions<Schedule> | undefined): Promise<Schedule[]> {
+    return Schedule.findAll(options);
+  }
+}
+
+export class SessionDataAccess implements IDataAccess<Session> {
+  public async create(values?: FilteredModelAttributes<Session>, options?: ICreateOptions): Promise<Session> {
+    return Session.create(values, options);
+  }  
+  
+  public async findOne(options?: IFindOptions<Session> | undefined): Promise<Session | null> {
+    return Session.findOne(options);
+  }
+
+  public async findAll(options?: IFindOptions<Session> | undefined): Promise<Session[]> {
+    return Session.findAll(options);
   }
 }
