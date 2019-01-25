59add215e84ae12392850308b521aed047f30cab
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-01-25 20:29:58 +0800

    backend subject: Implemented ISubject

--- backend/app/database/models/subject.ts
+++ backend/app/database/models/subject.ts
@@ -1,15 +1,24 @@
 import { Column, DataType, Model, PrimaryKey, Table } from "sequelize-typescript";
 
+import { ISubject } from "../../interfaces/model.columns/ISubject";
+
 @Table({
   tableName: "subjects",
   paranoid: true
 })
-export class Subject extends Model<Subject> {
+export class Subject extends Model<Subject> implements ISubject {
   @PrimaryKey
   @Column(DataType.STRING)
   public subjectCode: string;
 
+  @Column(DataType.STRING)
   public subjectTitle: string;
+
+  @Column(DataType.STRING)
+  public subjectUnits: number;
+  
+  @Column(DataType.STRING)
+  public subjectDescription?: string | undefined;
 }
 
 export default Subject;
