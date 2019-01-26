dd9726b7424d60c76c00e95c6e638c748d73f726
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2019-01-26 21:34:40 +0800

    backend init.models: Added new models to init of db

--- backend/app/database/init.models.ts
+++ backend/app/database/init.models.ts
@@ -1,15 +1,30 @@
 import { Sequelize } from "sequelize-typescript";
 
+import AcademicTime from "./models/academic.time";
+import RequiredSession from "./models/required.session";
+import Room from "./models/room";
+import Schedule from "./models/schedule";
+import Session from "./models/session";
+import Subject from "./models/subject";
 import User from "./models/user";
-import Token from "./models/user.details";
+import Token, { UserDetails } from "./models/user.details";
+import UserSchedule from "./models/user.schedule";
 
 /**
  * Adds all of the models to the given sequelize instance
  */
 export function initModels(sequelize: Sequelize) {
   sequelize.addModels([
-    User,
+    AcademicTime,
+    RequiredSession,
+    Room,
+    Schedule,
+    Session,
+    Subject,
     Token,
+    UserDetails,
+    UserSchedule,
+    User,
   ]);
 }
 
